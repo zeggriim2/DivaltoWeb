57490b53c7ea6a8855e9eb5b590c597e64f14f50
Zeggriim <lilian.dorazio@hotmail.fr>
2020-04-20 01:25:37 +0200

    Création du formulaire pour l'ajout des annonces.

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdController extends AbstractController
 {
     /**
      * Permet d'afficher tout les annonces
      * 
-     * @Route("/ads", name="ads")
+     * @Route("/ads", name="ads_index")
      * @return Response
      */
     public function index(AdRepository $repo)
@@ -26,13 +29,40 @@ class AdController extends AbstractController
     }
 
     /**
-     * Permet d'afficher une seule annonce
-     * 
-     * @Route("/ads/{slug}", name="ads_show ")
+     * permet de creé une annonce
+     *
+     * @Route("/ads/new", name="ads_create")
      * @return response
      */
-    public function show($slug, AdRepository $repo){
-        $ad = $repo->findOneBySlug($slug);
+    public function create(Request $request, ObjectManager $manager){
+        $ad = new Ad();
+
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+        // Test si le formulaire a été soumis et a été valide.
+        if ($form->isSubmitted() && $form->isValid()){
+
+            
+            $manager->persist($ad);
+            $manager->flush();
+        }
+
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+    
+    /**
+     * Permet d'afficher une seule annonce
+     * 
+     * @Route("/ads/{slug}", name="ads_show")
+     * @return response
+     */
+    public function show( Ad $ad){
+        // Je récupere l'annonce qui correspond au slug
+        // $ad = $repo->findOneBySlug($slug);
+        dump($ad);
         return $this->render('ad/show.html.twig',[
             'ad' => $ad
         ]);
