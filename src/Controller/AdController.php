de5d42044f3d547215a0821e3e01640ae0bad9e5
Zeggriim <lilian.dorazio@hotmail.fr>
2020-04-22 00:24:27 +0200

    Modification sur le formulaire new annonce
    - Ne pas être obliger de renseigne le slug
    - Ajoute de fonctionnalité d'ajout d'image dans le courrousel

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Entity\Image;
 use App\Form\AdType;
 use App\Repository\AdRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -38,12 +39,19 @@ class AdController extends AbstractController
     public function create(Request $request, EntityManagerInterface $manager){
         $ad = new Ad();
 
+
         $form = $this->createForm(AdType::class, $ad);
 
         $form->handleRequest($request);
 
         // Test si le formulaire a été soumis et a été valide.
         if ($form->isSubmitted() && $form->isValid()){
+
+            foreach($ad->getImages() as $image){
+                $image->setAd($ad);
+                $manager->persist($image);
+            }
+
             $manager->persist($ad);
             $manager->flush();
             
--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -3,14 +3,16 @@
 namespace App\Form;
 
 use App\Entity\Ad;
+use App\Form\ImageType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class AdType extends AbstractType
 {
@@ -20,34 +22,63 @@ class AdType extends AbstractType
 	 *
 	 * @param string $label
 	 * @param string $placeholder
+	 * @param array  $option
 	 * @return array
 	 */
-	private function getConfiguration($label, $placeholder) :array {
-		return [
+	private function getConfiguration($label, $placeholder, $option = []) :array {
+		return array_merge([
 				'label' => $label, 
 				'attr' => [
 					'placeholder' => $placeholder
 				]
-			];
+				], $option);
 	}
 
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
 		$builder
-			->add('title', TextType::class, $this->getConfiguration('Titre', "Tapez un super titre pour votre annonce !"))
-			->add('slug', TextType::class, $this->getConfiguration('Chaine URL', "Tapez l'adresse Web (automatique)."))
-			->add('coverImage', UrlType::class, $this->getConfiguration("Url de l'image principale", "Donnez l'adresse de l'image qui donne vraiment envie !"))
-			->add('introduction', TextType::class, $this->getConfiguration('Introduction', "Donnez une description globale de l'annonce."))
-			->add('contenu', TextareaType::class, $this->getConfiguration('Description détaillée', "Tapez une description qui donne vraiment envie de venir chez vous !"))
-			->add('price', MoneyType::class, $this->getConfiguration('Prix par nuit', "Indiquez le prix que vous voulez pour une nuit."))
-			->add('rooms', IntegerType::class, $this->getConfiguration("Nombre de chambre", "Le nombre de chambres disponibles."))
+			->add('title',
+					TextType::class, 
+					$this->getConfiguration('Titre', "Tapez un super titre pour votre annonce !")
+				)
+			->add('slug', 
+					TextType::class, 
+					$this->getConfiguration('Chaine URL', "Tapez l'adresse Web (automatique).", ['required' => false])
+				)
+			->add('coverImage', 
+					UrlType::class, 
+					$this->getConfiguration("Url de l'image principale", "Donnez l'adresse de l'image qui donne vraiment envie !")
+				)
+			->add('introduction', 
+					TextType::class, 
+					$this->getConfiguration('Introduction', "Donnez une description globale de l'annonce.")
+				)
+			->add('contenu',
+					TextareaType::class,
+					$this->getConfiguration("Description détaillée", "Tapez une description qui donne vraiment envie de venir chez vous!")
+			)
+			->add('price',
+					MoneyType::class,
+					$this->getConfiguration("Prix par nuit", "Indiquez le prix que vous voulez pour une nuit!")
+			)
+			->add('rooms',
+					IntegerType::class,
+					$this->getConfiguration("Nombre de chambre", "Le nombre de chambres disponibles.")
+			)
+			->add('images',
+					CollectionType::class,
+					[
+						'entry_type' => ImageType::class,
+						'allow_add' => true
+					]
+			)
 		;
 	}
 
 	public function configureOptions(OptionsResolver $resolver)
 	{
 		$resolver->setDefaults([
-			'data_class' => Ad::class,
+			'data_class' => Ad::class			
 		]);
 	}
 }
